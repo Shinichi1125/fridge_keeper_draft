b21b21783d385ab8a80dc210aab1e1f386c79018
stakagi <Shinichi.Takagi@ha.ax>
2020-01-12 15:01:11 +0200

    getting most of the styling issues with log in form fixed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,18 +2,23 @@ import React from "react";
 
 function Login(){
     return (
-      <div>
+      <div class="login-body">
         <form> 
-          <div class="form-group">
+          <div class="form-signin">
               <label for="email">Email</label>
               <input type="email" class="form-control" name="username"/>
           </div>
-          <div class="form-group">
+          <div class="form-signin">
               <label for="password">Password</label>
               <input type="password" class="form-control" name="password"/>
           </div>
           <button class="btn btn-dark">Log in</button>
-          <button class="btn btn-dark">Easy Log in</button>
+          <br></br>
+          <button class="btn btn-success">Easy Log in</button>
+          <br></br>
+          <p class="alert alert-info">↑ Just click "Easy Log in" and you can log in</p>
+
+          
         </form>
       </div> 
     );
