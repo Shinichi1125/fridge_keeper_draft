8e69bcfa7fae082abba3df3705db188816234242
stakagi <Shinichi.Takagi@ha.ax>
2020-01-15 10:37:50 +0200

    the logout button is now right justified and in the same line as the header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 function Header(prop){
     return <header>
-        <h1>Fridge Manager</h1>
-        <button 
-            className="btn btn-secondary"
-            onClick={prop.onClick}
-        >   
-            Log out
-        </button>
+        <h1 className="header-title">Fridge Manager
+            <span className="header-child1">
+                <button 
+                    className="btn btn-secondary header-btn"
+                    onClick={prop.onClick}
+                >   
+                    Log out
+                </button>
+            </span>
+        </h1>
     </header>
 }
 
